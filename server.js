9ddcea3d8bd561cb44b06d34db3c4778468ac18c
M-KALIRAJAN <kalirajanmurugaiya@gmail.com>
2025-09-04 15:25:50 +0530

    add route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import contactRoutes from "./routes/contactRoutes"
-
+import contactRoutes from './routes/contactRoutes.js';
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 app.use('/contacts',contactRoutes)
 
 app.get('/',(req,res)=>{
