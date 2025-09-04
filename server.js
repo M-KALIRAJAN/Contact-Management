ac9a18a515d755813ee9d8c8219b941e918174df
M-KALIRAJAN <kalirajanmurugaiya@gmail.com>
2025-09-04 16:20:26 +0530

    add route

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err));
 
